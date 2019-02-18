1bebd04de7434ab6433febb53f86f80a9bd2816e
eddyc2005 <32104510+eddyc2005@users.noreply.github.com>
2019-02-18 17:48:06 +0800

    'ok2'

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-// 引用linebot SDK
 var linebot = require('linebot');
 var express = require('express');
 
-
-// 填入辨識Line Channel的資訊
 var bot = linebot({
     channelId: '1647433696',
     channelSecret: 'f6c7469a8be7963b38a15729d432ebf9',
     channelAccessToken: 'e/dsgwo/uANnGgm5zw0ccc03ERo8GRswTCxTcA6/qSVVvt1eHk/wxV69E6u7eQsH3Pnnvez+ZV13yh67ssxok5jXqNilT4EG/TRNA8ZucA374z5E/Yc+OrWJC8m12geqUYOkbgtDH7eR2DHPImOLSwdB04t89/1O/w1cDnyilFU='
 });
 
-// 當有人傳送訊息給Bot時
-bot.on('message', function (event) {
-    // event.message.text是使用者傳給bot的訊息
-    // 準備要回傳的內容
-    var replyMsg = `Hello你剛才說的是:${event.message.text}`;
-    if(event.message.text == '嗨'){
-        event.reply('幹').then(function (data) {
-            // 當訊息成功回傳後的處理
-        }).catch(function (error) {
-            // 當訊息回傳失敗後的處理
-        });
-    }else{
-        event.reply(replyMsg).then(function (data) {
-            // 當訊息成功回傳後的處理
-        }).catch(function (error) {
-            // 當訊息回傳失敗後的處理
+//這一段的程式是專門處理當有人傳送文字訊息給LineBot時，我們的處理回應
+bot.on('message', function(event) {
+    if (event.message.type = 'text') {
+        var msg = event.message.text;
+        //收到文字訊息時，直接把收到的訊息傳回去
+        event.reply(msg).then(function(data) {
+            // 傳送訊息成功時，可在此寫程式碼
+            console.log(msg);
+        }).catch(function(error) {
+            // 傳送訊息失敗時，可在此寫程式碼
+            console.log('錯誤產生，錯誤碼：'+error);
         });
     }
 });
